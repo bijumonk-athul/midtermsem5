d28cf2edacef84f8172c39751aeb54da78ae6cb4
bijumonk-athul <bijumonk@sheridancollege.ca>
2026-02-09 17:42:22 -0500

    Initial commit by Athul Bijumon Kolothu

---
+++ src/main/java/com/mycompany/midterm/ProductManager.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.midterm;
+
+/**
+ *
+ * @author Athul
+ */
+public class ProductManager {
+
+     private String name;
+    private double price;
+    
+}
